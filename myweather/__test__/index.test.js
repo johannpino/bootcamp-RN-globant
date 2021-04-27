64e61294586778d8c30d6e3459328c0094776c9f
Tiago Romero <quetzaltiago@gmail.com>
2021-04-27 15:47:07 -0300

    index tested

--- myweather/__test__/index.test.js
+++ myweather/__test__/index.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen, cleanup } from "@testing-library/react";
+import { cleanup } from "@testing-library/react";
 import WeatherProvider from "../context/weatherContext";
 import Home from "../pages/index";
 
