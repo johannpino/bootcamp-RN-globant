98848c0ac918b03398823069eb7904cf0ca9882b
Piero-marino <67409264+Piero-marino@users.noreply.github.com>
2021-05-03 22:35:47 -0300

    boilerplate, dependencies added

---
+++ athena/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  
+  return (
+    <>
+    
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
