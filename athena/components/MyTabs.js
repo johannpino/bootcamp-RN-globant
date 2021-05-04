c8b92d33e433a67c772f596428ade8b73af472fc
Tiago Romero <quetzaltiago@gmail.com>
2021-05-04 11:36:38 -0300

    navbar icons added

--- athena/components/MyTabs.js
+++ athena/components/MyTabs.js
@@ -1,19 +1,47 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import {View, Text, StyleSheet} from 'react-native'
-import Home from './Home'
-import Projects from './Projects'
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Home from './Home';
+import Projects from './Projects';
+import Profile from './Profile';
+import Settings from './Settings';
 
-const Tab  = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
-    return (
-        <Tab.Navigator initialRouteName="Home" >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Projects" component={Projects} />
-        </Tab.Navigator>
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        showLabel: false,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          color = 'white';
+          size = 30;
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Projects') {
+            iconName = focused ? 'folder-open' : 'folder-open-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person-circle' : 'person-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
 
-    )
-}
+          // You can return any component that you like here!
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Projects" component={Projects} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+};
 
-export default MyTabs
+export default MyTabs;
---
+++ athena/components/Settings.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+
+const Settings = () => {
+  return <ScrollView style={styles.container}></ScrollView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1B1B1B',
+    padding: '5%',
+  },
+});
+
+export default Settings;
