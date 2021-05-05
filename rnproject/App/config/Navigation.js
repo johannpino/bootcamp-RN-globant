dade421f7a81c004fd49e3be0896ef55567d7ee6
Latuz <fstuardom@gmail.com>
2021-05-05 10:03:01 -0400

    Update config de Screens/ReactNav

--- rnproject/App/config/Navigation.js
+++ rnproject/App/config/Navigation.js
@@ -3,13 +3,45 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from '../screens/Home';
-import Comunas from '../screens/Comunas';
+import Viajes from '../screens/Viajes';
+import Busqueda from '../screens/Busqueda';
+import C19 from '../screens/C19';
+import Comisaria from '../screens/Comisaria';
+import FAQ from '../screens/FAQ';
 
 const MainStack = createStackNavigator();
 const MainStackScreen = () => (
   <MainStack.Navigator>
-    <MainStack.Screen name="MyAppInfo" component={Home} />
-    <MainStack.Screen name="Comunas" component={Comunas} />
+    <MainStack.Screen
+      name="Home test"
+      component={Home}
+      options={({headerTitleAlign: 'center'}, {headerShown: true})}
+    />
+    <MainStack.Screen
+      name="Viajes"
+      component={Viajes}
+      options={{headerTitleAlign: 'center'}}
+    />
+    <MainStack.Screen
+      name="Busqueda"
+      component={Busqueda}
+      options={{headerTitleAlign: 'center'}}
+    />
+    <MainStack.Screen
+      name="C19"
+      component={C19}
+      options={{headerTitleAlign: 'center'}}
+    />
+    <MainStack.Screen
+      name="Comisaria"
+      component={Comisaria}
+      options={{headerTitleAlign: 'center'}}
+    />
+    <MainStack.Screen
+      name="FAQ"
+      component={FAQ}
+      options={{headerTitleAlign: 'center'}}
+    />
   </MainStack.Navigator>
 );
 
