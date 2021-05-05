24dce22f389dab40853550e0fda43f6c19a28d26
Latuz <fstuardom@gmail.com>
2021-05-05 10:01:12 -0400

    Added viajes con Input de usuario

--- rnproject/App/components/UserInput.js
+++ rnproject/App/components/UserInput.js
@@ -6,16 +6,40 @@ import {
   StyleSheet,
   Text,
 } from 'react-native';
+import colors from '../constants/colors';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    marginHorizontal: 10,
+    marginVertical: 20,
+    borderRadius: 10,
+    flexDirection: 'row',
+  },
+  button: {
+    padding: 15,
+    borderRightColor: colors.border,
+    borderRightWidth: 1,
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: colors.blue,
+  },
+  input: {
+    flex: 1,
+    padding: 12,
+    color: colors.lightcolor,
+  },
+});
 
-export const UserInput = ({text, value, onButtonPress}) => {
+export const UserInput = ({text, placeholder, onButtonPress}) => {
   return (
-    <View>
-      <TouchableOpacity onPress={onButtonPress}>
-        <Text>{text}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onButtonPress} style={styles.button}>
+        <Text style={styles.buttonText}>{text}</Text>
       </TouchableOpacity>
-      <TextInput />
+      <TextInput style={styles.input} placeholder={placeholder} />
     </View>
   );
 };
---
+++ rnproject/App/screens/Viajes.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  StatusBar,
+  Image,
+  Dimensions,
+  Text,
+} from 'react-native';
+import colors from '../constants/colors';
+import {UserInput} from '../components/UserInput';
+
+const screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.blue,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: screen.width * 0.7,
+    height: screen.width * 0.7,
+  },
+  titleComunas: {
+    color: colors.white,
+    fontSize: 18,
+
+    textAlign: 'center',
+  },
+});
+
+export default () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Text style={styles.titleComunas}>
+          ¡Bienvenido a tu ayudante de viajes!
+        </Text>
+        <Image
+          style={styles.logo}
+          source={require('../assets/images/icono2.png')}
+        />
+      </View>
+      <UserInput text="DESDE" placeholder="Comuna de Inicio" />
+      <UserInput text="HACIA" placeholder="Comuna de destino" />
+    </View>
+  );
+};
