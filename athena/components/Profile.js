5f71d9ca844aa80751041d25f8e822b884b9be84
Tiago Romero <quetzaltiago@gmail.com>
2021-05-07 16:30:45 -0300

    added profile view

--- athena/components/Profile.js
+++ athena/components/Profile.js
@@ -1,21 +1,45 @@
 import React, { useContext } from 'react';
-import { ScrollView, StyleSheet, Button } from 'react-native';
-import AuthContext from '../context/auth/authContext';
+import { Pressable, StyleSheet, View, Text } from 'react-native';
+import ProfilePicture from './ProfilePicture';
+import FireBaseContext from '../context/firebase/firebaseContext';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: '5%',
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  editBtn: {
+    borderWidth: 2,
+    borderColor: '#FFF',
+    marginVertical: 20,
+    backgroundColor: '#010101',
+    borderRadius: 6,
+    padding: 12,
+  },
+  editText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
 const Profile = () => {
-  const authContext = useContext(AuthContext);
-  const { logout } = authContext;
+  const firebaseContext = useContext(FireBaseContext);
+  const { user } = firebaseContext;
+
   return (
-    <ScrollView style={styles.container}>
-      <Button title="logout" onPress={() => logout()} />
-    </ScrollView>
+    <View style={styles.container}>
+      <ProfilePicture user={user} />
+      <Text style={styles.name}>{user.displayName}</Text>
+      <Pressable style={styles.editBtn}>
+        <Text style={styles.editText}>EDITAR PERFIL</Text>
+      </Pressable>
+    </View>
   );
 };
 
