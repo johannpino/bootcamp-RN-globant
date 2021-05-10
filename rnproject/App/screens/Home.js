52410d0b103af85bd8cc4c0a171f2bd417a78420
Latuz <fstuardom@gmail.com>
2021-05-10 13:07:48 -0400

    Cambio por Drawer plus welcome message

--- rnproject/App/screens/Home.js
+++ rnproject/App/screens/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {SafeAreaView, ImageBackground, StatusBar} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import {
+  View,
+  ImageBackground,
+  StatusBar,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 import colors from '../constants/colors';
-import {RowItem, RowSeparator} from '../components/RowItem';
+// import {RowItem, RowSeparator} from '../components/RowItem';
 
 /* const openURL = url => {
   return Linking.openURL(url).catch(() => {
@@ -10,55 +16,52 @@ import {RowItem, RowSeparator} from '../components/RowItem';
   });
 }; */
 
-export default ({navigation}) => {
+/* Prop para la función cuando se usa Screens normales con touch {navigation} */
+
+const screen = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 50,
+    alignContent: 'center',
+    paddingBottom: 50,
+  },
+  p: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    alignContent: 'center',
+    padding: 30,
+    paddingVertical: 20,
+  },
+  background: {
+    width: screen.width * 1,
+    height: screen.height * 1,
+    position: 'relative',
+    alignItems: 'center',
+    paddingTop: 200,
+  },
+});
+
+export default () => {
   return (
-    <SafeAreaView>
+    <View styles={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.text} />
+
       <ImageBackground
         source={require('../assets/images/background.png')}
-        style={{width: '100%', height: '100%'}}
-        imageStyle={{opacity: 0.3}}
+        style={styles.background}
+        imageStyle={{opacity: 0.4}}
       >
-        <RowItem
-          text="¿En que fase se encuentra mi comuna?"
-          onPress={() => alert('TEST ON PRESS!')}
-          rightIcon={
-            <Icon name="map-marker-alt" size={20} color={colors.red} />
-          }
-        />
-        <RowSeparator />
-        <RowItem
-          text="Busqueda por Comuna"
-          onPress={() => navigation.push('Busqueda')}
-          rightIcon={<Icon name="search" size={20} color="black" />}
-        />
-        <RowSeparator />
-        <RowItem
-          text="¿Quieres viajar?"
-          onPress={() => navigation.push('Viajes')}
-          rightIcon={<Icon name="route" size={20} color="black" />}
-        />
-        <RowSeparator />
-
-        <RowItem
-          text="C19: Pasaporte Sanitario. Obtenlo Aqui"
-          onPress={() => navigation.push('C19')}
-          rightIcon={<Icon name="passport" size={20} color="black" />}
-        />
-        <RowSeparator />
-        <RowItem
-          text="¿Permiso de desplazamiento? ¡Presiona Aqui!"
-          onPress={() => navigation.push('Comisaria')}
-          rightIcon={<Icon name="id-card" size={20} color="black" />}
-        />
-        <RowSeparator />
-        <RowItem
-          text="Preguntas Frecuentes"
-          onPress={() => navigation.push('FAQ')}
-          rightIcon={<Icon name="question" size={20} />}
-        />
-        <RowSeparator />
+        <Text style={styles.title}>¡Bienvenido!</Text>
+        <Text style={styles.p}>
+          ¡Tú nueva experienca para realizar tramites online acaba de comenzar!
+        </Text>
       </ImageBackground>
-    </SafeAreaView>
+    </View>
   );
 };
