1899da4e6a38b89ea3ec47cd4c2f42b0d8d404ca
Latuz <fstuardom@gmail.com>
2021-05-10 13:06:59 -0400

    Added Search logic && custom Alert

--- rnproject/App/screens/Busqueda.js
+++ rnproject/App/screens/Busqueda.js
@@ -1,21 +1,114 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {UserInput} from '../components/UserInput';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  Pressable,
+  Alert,
+} from 'react-native';
+import Papa from 'papaparse';
 import colors from '../constants/colors';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.blue,
     flex: 1,
+    backgroundColor: colors.blue,
+    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: 300,
+    borderColor: 'red',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 8,
+  },
+  search: {
+    paddingBottom: 30,
+    fontSize: 25,
+    padding: 10,
+  },
+  list: {
+    paddingTop: 10,
+  },
+  text: {
+    fontSize: 20,
   },
 });
 
-export default () => {
+const Filter = () => {
+  const [data, setData] = useState('');
+  const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState([]);
+  const removeAccents = str => {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  };
+
+  const handleChange = e => {
+    setQuery(e);
+  };
+
+  useEffect(() => {
+    Papa.parse(
+      'https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto74/paso_a_paso_T.csv',
+      {
+        download: true,
+        complete: results => {
+          console.log(results);
+          setData(results);
+        },
+      },
+    );
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const allComunas = data.data[3];
+      const result = allComunas.filter(comuna => {
+        const lower = removeAccents(comuna.toLowerCase());
+        return lower.includes(query.toLowerCase());
+      });
+
+      setFilter(result);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const faseActual = item => {
+    const index = parseInt(data.data[3].indexOf(item), 10);
+    return data.data[data.data.length - 2][index];
+  };
+
+  const handlePress = item => {
+    Alert.alert(
+      '¡Hola! Te informamos que',
+      `La comuna se encuentra en: ${faseActual(item)}`,
+      [
+        {
+          text: 'Aceptar',
+          style: 'ok',
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text> SCREEN PARA SEARCH </Text>
-      <UserInput placeholder="Ingresa la comuna" />
+      <Text style={styles.search}>Porfavor Ingresa la Comuna que buscas</Text>
+      <TextInput onChangeText={handleChange} style={styles.input} />
+      <ScrollView style={styles.list}>
+        {filter.map((item, idx) => (
+          <Pressable onPress={() => handlePress(item)} key={idx}>
+            <Text style={styles.text}>{item}</Text>
+          </Pressable>
+        ))}
+      </ScrollView>
     </View>
   );
 };
+
+export default Filter;
